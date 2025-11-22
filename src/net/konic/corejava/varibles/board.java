2a2f521ed72cdfc1234a6a6a726ad5b22fe50fea
vasifnawazshaik <vasifnawazshaik@gmail.com>
2025-11-22 10:47:08 +0530

    dev

---
+++ src/net/konic/corejava/varibles/board.java
@@ -0,0 +1,22 @@
+package net.konic.corejava.varibles;
+
+public class board {
+
+		public static void main(String[] args) {
+			
+		
+		String boardname = "lite";
+		int length = 12;
+		float wedith = 15;
+		System.out.println("boardname :"+ boardname);
+		System.out.println("length :"+ length);
+		System.out.println("wedith :"+ wedith);
+		
+		
+
+		}
+
+	}
+
+
+
